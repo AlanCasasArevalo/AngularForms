1885d6c24d06db467a7c0a194b47d27a2998650d
Alan Casas <alancasasarevalo@gmail.com>
2018-04-15 20:17:53 +0200

    Some custom validations was added (this validations should be into file apart) Async validations was added too some reactive values were subscribe to know when this values were modified Nickname, Password and Confirm password were added

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-data',
@@ -25,17 +26,41 @@ export class DataComponent implements OnInit {
 
     this.form = new FormGroup({
       'fullName' : new FormGroup({
-        'name': new FormControl('', [Validators.required, Validators.minLength(3)]),
-        'lastname': new FormControl('', Validators.required)
+        'name': new FormControl('', [ Validators.required, Validators.minLength(3) ]),
+        'lastname': new FormControl('', [ Validators.required, this.noLastNameRepeated ])
       }),
       'email': new FormControl('', [Validators.required, Validators.email]),
       'hobbies': new FormArray([
         new FormControl('Comer', Validators.required)
-      ])
+      ]),
+      'password' : new FormControl('', Validators.required),
+      'confirmPassword' : new FormControl(),
+
+      // Assync validations
+      'nickname' : new FormControl('', Validators.required, this.isUserIntoDataBase),
     });
+
+
+    this.form.controls['confirmPassword'].setValidators([
+      Validators.required,
+      // when we put .bind( this.form ) we do the this object is form in all file
+      this.confirmPasswords.bind( this.form )
+    ]);
 
     // This just work IF and just IF your object properties and form properties are the same
     // this.form.setValue(this.user);
+
+    this.form.controls['nickname'].valueChanges.subscribe( data => {
+      console.log(data);
+    });
+
+    this.form.controls['nickname'].statusChanges.subscribe( data => {
+      console.log(data);
+    });
+
+    // this.form.valueChanges.subscribe( data => {
+    //   console.log(data);
+    // });
 
   }
 
@@ -43,6 +68,48 @@ export class DataComponent implements OnInit {
     (<FormArray>this.form.controls['hobbies']).push(
       new FormControl('Leer', Validators.required)
     );
+  }
+
+  // This validations should be into apart file
+  noLastNameRepeated( control: FormControl ): {[string: string]: boolean } {
+    if (control.value === 'casas') {
+      return {
+        nocasas: true
+      };
+    }
+
+    return null;
+
+  }
+  // This validations should be into apart file
+  isUserIntoDataBase( control: FormControl ): Promise<any> | Observable<any> {
+
+    const promise = new Promise( ( resolve, reject ) => {
+      setTimeout(() => {
+        if (control.value === 'Javito') {
+          resolve( { exits : true} );
+        } else {
+          resolve( null );
+        }
+      }, 3000);
+    });
+
+    return promise;
+  }
+
+  // This validations should be into apart file
+  confirmPasswords( control: FormControl ): {[string: string]: boolean } {
+
+    const form: any = this;
+
+    if (control.value !== form.controls['password'].value) {
+      return {
+        noTheSamePassword: true
+      };
+    }
+
+    return null;
+
   }
 
   saveData() {
@@ -56,14 +123,14 @@ export class DataComponent implements OnInit {
     this.form.reset(this.user);
 
     // This reset the form but without any values
-    this.form.reset({
-      'fullName' : {
-        'name': 'Alan',
-        'lastname': 'Casas'
-      },
-      'email': 'test@gmail.com',
-      'hobbies' : ''
-    });
+    // this.form.reset({
+    //   'fullName' : {
+    //     'name': 'Alan',
+    //     'lastname': 'Casas'
+    //   },
+    //   'email': 'test@gmail.com',
+    //   'hobbies' : ''
+    // });
 
   }
 
